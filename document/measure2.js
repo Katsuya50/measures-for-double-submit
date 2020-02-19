b41cf27dab15614e62b7ef639c3ecfab063d8a22
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-19 19:14:19 +0900

    軽微な修正

--- document/measure2.js
+++ document/measure2.js
@@ -1,6 +1,6 @@
 // ボタンの無効化
 $(function(){
 	$("#btn").on("click",function(){
-		$(this).css("cursor","default").css("opacity","0.2");
+		$(this).prop("disabled",true).css("cursor","default").css("opacity","0.2");
 	});
 });
