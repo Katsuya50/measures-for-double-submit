2c71af0a2db2e40410b4b33f711aac397481a269
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-20 12:12:49 +0900

    画面遷移の実装

--- document/measure3.js
+++ document/measure3.js
@@ -4,7 +4,11 @@ var click = 0;
 function warning(){
 	if(click == 0){
 		click = 1;
+		setTimeout(() => {
+			document.myform.submit();
+		}, 5000);
 	}else{
 		alert("ただいま処理中です。\nこのままお待ちください。");
+		return false;
 	}
 }
