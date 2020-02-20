73d2c7057412c3ead19e8dea03fd5786f014d7d1
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-20 22:27:16 +0900

    軽微な修正

--- document/measure3.js
+++ document/measure3.js
@@ -4,11 +4,7 @@ var click = 0;
 function warning(){
 	if(click == 0){
 		click = 1;
-		setTimeout(() => {
-			document.myform.submit();
-		}, 5000);
 	}else{
 		alert("ただいま処理中です。\nこのままお待ちください。");
-		return false;
 	}
 }
